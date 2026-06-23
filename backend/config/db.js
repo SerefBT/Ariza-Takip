bd439d19556ca06d7302b5aaf2ffa2e43d8b7db2
SerefBT <cosar.bayat@gmail.com>
2026-06-23 14:52:15 +0300

    Change MONGO_URI to DB_LINK to force Render cache invalidation

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ariza-takip');
+    const conn = await mongoose.connect(process.env.DB_LINK || 'mongodb://localhost:27017/ariza-takip');
     console.log(`MongoDB Bağlantısı Başarılı: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Bağlantı Hatası: ${error.message}`);
